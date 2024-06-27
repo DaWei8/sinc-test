18d82c0d191dada2808c6748628803fafe77ee49
KingDavid <dayvhidlee@gmail.com>
2024-06-27 09:08:24 +0100

    removed redundant dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/ui/Navbar";
 import Footer from "@/components/ui/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "SINC Partners",
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={montserrat.className}>
         <div className=" flex w-[100dvw] max-w-[100dvw] items-center flex-col min-h-[100dvh] bg-[#F4F4F4] ">
           <Navbar />
           {children}
